64ba26513ad4cc8e2b81307dce3de0e64d88abf5
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-14 09:21:27 +0530

    node js, express , and postman

---
+++ backend-server/server.js
@@ -0,0 +1,26 @@
+
+const express = require('express'); 
+const app = express();
+const bodyParser = require('body-parser')
+
+
+// use to parse rwq.body data
+app.use(bodyParser.json())
+
+app.get('/', (req , res) => {
+    res.send('<h1>Server creation using express in Node js</h1>');
+})
+
+
+
+app.post('/profile/user' , (req , res) => {
+    const { username , password} = req.body;
+    console.log(username);
+    console.log(password);
+    res.send("User created successfully");
+})
+
+
+app.listen(3000 , () => {
+    console.log('Server is running on port 3000');  
+}) 

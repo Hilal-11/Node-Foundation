22cf7fa2b70037e29607ba5540a55862b57240b9
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-19 07:11:55 +0530

    done

--- Todo App/index.js
+++ Todo App/index.js
@@ -5,10 +5,17 @@ require("dotenv").config();
 const connectDB = require("./config/database")
 const PORT = process.env.PORT || 4000;
 
+const todoRoutes = require("./routes/todos")
+
+// middleware to parse body data
+app.use(express.json());
 
 app.get('/' , (req , res) => {
     res.send("<h1>This is my Home route of Express application</h1>");
 })
+
+
+app.use("/api/v1" , todoRoutes)
 
 app.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`)

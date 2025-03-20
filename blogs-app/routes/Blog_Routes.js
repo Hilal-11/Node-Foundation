6b64a23e77c6da3a26b68ddf8f9525275b316f2e
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-20 07:17:35 +0530

    controllers and routes..

--- blogs-app/routes/Blog_Routes.js
+++ blogs-app/routes/Blog_Routes.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose')
+
 const express = require('express')
 const router = express.Router();
 
 const createBlog = require("../controllers/createBlog")
 const getBlogs = require("../controllers/getBlog")
+const getBlog = require("../controllers/getBlog")
 const updateBlog = require("../controllers/updateBlog")
 const deleteBlog = require("../controllers/deleteBlog")
 
 
-router.get("/createBlog" , createBlog);
-router.post("/getBlogs" , getBlogs);
-router.put("/updateBlog" , updateBlog);
-router.delete("/deleteBlog" , deleteBlog);
+router.get("/getBlogs" , getBlogs);
+router.get("/getBlog/:id" , getBlog)
+router.post("/createBlog" , createBlog);
+router.put("/updateBlog/:id" , updateBlog);
+router.delete("/deleteBlog/:id" , deleteBlog);
 
 
 module.exports = router;

b87feed5309f120c2e2dc76d752bea65e6cf8e7c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-20 07:45:43 +0530

    fetch single blog

---
+++ blogs-app/controllers/getBlog.js
@@ -0,0 +1,26 @@
+
+const Blog = require('../models/Blog')
+
+const getBlog = async (req , res) => {
+    try{
+        const {id} = req.body;
+        const response = Blog.find({_id : id})
+        res.status(200).json({
+            success: true,
+            data: response,
+            message: "Successfully fetch the Blog of given is"
+        })
+
+    }catch(error) {
+        console.error(error.message);
+        console.log("failed to fetch blog of this id")
+        res.status(404).json({
+            success: false,
+            error: error.message,
+            message: "failed to fetch blog of given id"
+        })
+    }
+
+}
+
+module.exports = getBlog;

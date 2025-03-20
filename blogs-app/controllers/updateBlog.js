6c95649a31733ad352fafc7337ffe96f795e1662
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-20 08:00:46 +0530

    update controller

---
+++ blogs-app/controllers/updateBlog.js
@@ -0,0 +1,29 @@
+
+const Blog = require('../models/Blog');
+
+const updateBlog = async (req , res) => {
+    try{
+        const {id} = req.parame;
+        const {title , aboutBlog} = req.body
+        const update = await Blog.findByIdAndUpdate(
+            {_id: id},
+            {title , aboutBlog , updatedAt: Date.now()}
+        )
+        res.status(200).json({
+            success: true,
+            data: update,
+            message: "blog successfully updated"
+        })
+
+    }catch(error) {
+        console.error(error.message);
+        console.log("failed to update blog")
+        res.status(500).json({
+            success: false,
+            error: error.message,
+            message: "failed to update blog"
+        })
+    }
+}
+
+module.exports = updateBlog

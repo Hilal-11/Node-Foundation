6c8b2652adb9c29426d6de7f23497329cc902d14
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-20 10:58:44 +0530

    frontend and backend connect and rendering

---
+++ BackendDev/Backend/main.js
@@ -0,0 +1,28 @@
+
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.get("/" , (req , res) => {
+    res.send("<h1>Wel come frontend and backend</h1>")
+})
+
+app.get("/jokes", (req , res) => {
+    const jokes = [
+        { id: 1, title: "Why don't scientists trust atoms?", joke: "Because they make up everything!" },
+        { id: 2, title: "Why did the scarecrow win an award?", joke: "Because he was outstanding in his field!" },
+        { id: 3, title: "Why don't skeletons fight each other?", joke: "They don't have the guts." },
+        { id: 4, title: "What do you call fake spaghetti?", joke: "An impasta!" },
+        { id: 5, title: "Why did the bicycle fall over?", joke: "Because it was two-tired!" },
+        { id: 6, title: "What do you call cheese that isn't yours?", joke: "Nacho cheese!" },
+        { id: 7, title: "Why couldn't the leopard play hide and seek?", joke: "Because he was always spotted." },
+        { id: 8, title: "What do you call a bear with no teeth?", joke: "A gummy bear!" },
+        { id: 9, title: "Why did the golfer bring two pairs of pants?", joke: "In case he got a hole in one." },
+        { id: 10, title: "Why don't eggs tell jokes?", joke: "Because they'd crack each other up!" },
+    ]
+    res.send(jokes)
+})
+
+app.listen(PORT , () => {
+    console.log(`app is running on port ${PORT}`)
+})
---
+++ BackendDev/Frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+function App() {
+
+
+  const [jokes , setJokes] = useState([])
+  useEffect(() => {
+    // http://localhost:3000/jokes
+    axios.get('http://localhost:3000/jokes')
+    .then((response) => {
+      setJokes(response.data)
+      console.log(response.data)
+    }).catch((error) => {
+
+      console.log(error.message)
+      console.log("Data fetched failed ! sorry")
+    })
+  },[])
+
+
+  return (
+    <div>
+      <h1>Jokes App</h1>
+      <div>
+      <p>jokes: {jokes.length}</p>
+      {
+        jokes.map((joke , index) => (
+          <div key={joke.id}>
+            <h2>{joke.title}</h2>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+      </div>
+    </div>
+  )
+}
+
+export default App

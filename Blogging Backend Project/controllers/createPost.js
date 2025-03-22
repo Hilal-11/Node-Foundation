9a391e02204baf18990132a7e997bb82f450340f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-22 07:34:16 +0530

    post controller createPost.js

--- Blogging Backend Project/controllers/createPost.js
+++ Blogging Backend Project/controllers/createPost.js
@@ -1,3 +1,25 @@
 
-
 const BlogSchema = require('../models/Blog_model_schema')
+
+const createPost = async (req , res) => {
+    try{
+        const { title , description } = req.body;
+        const response = await BlogSchema.create({title , description})
+
+        res.status(200).json({
+            success: true,
+            data: response,
+            message: "successfully create a post"
+        })
+    }catch(error) {
+        console.log(error.message)
+        console.log("Failed to create a post ! Oops")
+        res.status(500).json({
+            success: false,
+            error: error.message,
+            message: "Failed to create a post Oops"
+        })
+    }
+}
+
+module.exports = createPost 

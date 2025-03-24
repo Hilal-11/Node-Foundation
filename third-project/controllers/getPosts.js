f9ab38653d88928ab28408b0b697a0887cb9d9cf
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-24 08:27:45 +0530

    get posts controller is done

---
+++ third-project/controllers/getPosts.js
@@ -0,0 +1,26 @@
+
+const Post = require('../models/PostModel')
+
+const getPosts = async (req , res) => {
+    try{    
+        const response = await Post.find({})
+        
+        res.status(200).json({
+            success: true,
+            data: response,
+            message: "Successfully fetched the posts from DB"
+        })
+    }catch(error){
+        console.log(error.message)
+        console.log("Failed  to fetch posts from DB")
+
+        res.status(500).json({
+            success: false,
+            error: error.message,
+            message: "Failed to fetch the posts from DB"
+        })
+    }
+}
+
+
+module.exports = getPosts
--- third-project/routes/app_routes.js
+++ third-project/routes/app_routes.js
@@ -7,10 +7,9 @@ const createPost = require('../controllers/createPost')
 const getPosts = require('../controllers/getPosts')
 const comments = require('../controllers/comments')
 const like = require('../controllers/like')
-const dislike = require('../controllers/dislike')
 
 router.get('/dummyPage', dummyPage)
-// router.get('/getPosts' , getPosts);
+router.get('/getPosts' , getPosts);
 router.post('/createPost', createPost)
 router.post('/comments/comment', comments)
 router.post('/likes/like', like)

878cc595626f4d7ef7fba2b53661a95893fbe6ce
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-11 23:11:39 +0530

    db connection

---
+++ Backend Practice A/config/database.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const connectDB = () => {
+    mongoose.connect(MONGODB_URL , {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("MongoDB connect successfully...")
+    }).catch((error) => {
+        console.log(error.message);
+    })
+}
+
+module.exports = connectDB;

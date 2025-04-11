c8bd2910196648b61c6486573d8012c164d41121
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-12 00:49:27 +0530

    dislike controller

---
+++ Backend Practice A/controllers/dislike.js
@@ -0,0 +1,28 @@
+const BlogSchema = require('../models/database_schemas_model');
+
+const dislike = async (req , res) => {
+    try{
+        const { id , isLike } = req.params();
+        const response = await BlogSchema.findByIdAndUpdate(
+            { _id: id },
+            { isLike, updatedAt: Date.now()}
+        )
+
+        res.status(200).json({
+            success: true,
+            data: response,
+            message: "Successfully dislike the post"
+        })
+    }catch(error) {
+        console.log(error.message)
+        console.log("Failed to dislike's the post")
+
+        res.status(500).json({
+            success: false,
+            error: error.message,
+            message: "Failed to dislike the post"
+        })
+    }
+}
+
+module.exports = dislike;

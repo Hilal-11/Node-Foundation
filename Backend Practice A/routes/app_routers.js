0226f89355c0ae7e78b50034c7737044b25a7114
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-11 23:32:10 +0530

    controlers

---
+++ Backend Practice A/routes/app_routers.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const router = express.Router();
+
+const getPosts = require('../controllers/getPosts')
+const createPost = require('../controllers/createPost')
+const like = require('../controllers/like')
+const dislike = require('../controllers/dislike')
+const commit = require('../controllers/commit')
+
+router.get();
+router.post();
+router.put();
+router.put();
+router.delete();

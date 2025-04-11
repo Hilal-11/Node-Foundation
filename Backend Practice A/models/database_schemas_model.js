113eb1f7f59c4955196a459e25a18c665e4fe484
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-11 23:49:17 +0530

    schna model

--- Backend Practice A/models/database_schemas_model.js
+++ Backend Practice A/models/database_schemas_model.js
@@ -3,22 +3,36 @@ const mongoose = require('mongoose')
 const Schema = mongoose.scheme(
     {
         title: {
-
+            type: String,
+            require: true,
+            maxLength: 100,
         },
         description: {
-
+            type: String,
+            require: true,
+            maxLength: 500,
         },
         isLike: {
-
+            type: Boolean,
+            require: true,
+            default: false,
         },
         comment:{
-
+            type: String,
+            required: true,
+            maxLength: 100,
+            default: "Your Comment"
+            
         },
         createdAt: {
-
+            type: Date,
+            required: true,
+            default: Date.now(),
         },
         updatedAt: {
-
+            type: Date,
+            required: true,
+            default: Date.now()
         }
     }
 )

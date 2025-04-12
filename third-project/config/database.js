028c2db3dc2cbb7e385b893c4a21b11432b25b4d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-12 22:27:45 +0530

    backend ...

--- third-project/config/database.js
+++ third-project/config/database.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 const MONGO_URI = process.env.MONGO_URI
 const connectDB = () => {
     mongoose.connect(MONGO_URI ,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true
     })
     .then(() => {
         console.log("Database connection successfully done")
